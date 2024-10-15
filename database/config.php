6dce51b37a6637e4eaa80e3c338b6dbe954a7155
Jaydev1824 <jaydev655423@gmail.com>
2024-10-15 10:34:45 -0600

    Update config.php

--- database/config.php
+++ database/config.php
@@ -5,5 +5,5 @@
 $host       = getenv('AZURE_MYSQL_HOST');
 $username   = getenv('AZURE_MYSQL_USERNAME');
 $password   = getenv('AZURE_MYSQL_PASSWORD');
-$db_name     = getenv('DB_DATABASE');
+$db_name     = getenv('AZURE_MYSQL_DBNAME');
 $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
